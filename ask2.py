7d7d77986d4cf3509da3d825cf8118cf99f4e0e0
Spyridwn <32803267+Spyridwn@users.noreply.github.com>
2017-10-15 00:07:19 +0300

    Add files via upload

---
+++ ask2.py
@@ -0,0 +1,93 @@
+
+
+
+arithmoi = raw_input('Dwse 24 arithmous xwrismenous me komma')
+arithmoi = arithmoi.split(",")
+arithmoi = map(int, arithmoi)
+
+print(arithmoi)
+
+a = arithmoi[0]
+b = arithmoi[1]
+c = arithmoi[2]
+d = arithmoi[3]
+e = arithmoi[4]
+f = arithmoi[5]
+max = a 
+if (max < b):
+    max = b
+if (max < c):
+    max = c
+if (max < d):
+    max = d
+if (max < e):
+    max = e
+if (max < f):
+    max = f
+
+
+
+g = arithmoi[6]
+i = arithmoi[7]
+j = arithmoi[8]
+k = arithmoi[9]
+l = arithmoi[10]
+m = arithmoi[11]
+tra = g
+if (tra < i):
+    tra = i
+if (tra < j):
+    tra = j
+if (tra < k):
+    tra = k
+if (tra < l):
+    tra = l
+if (tra < m):
+    tra = m
+
+
+
+
+n = arithmoi[12]
+p = arithmoi[13]
+q = arithmoi[14]
+r = arithmoi[15]
+s = arithmoi[16]
+t = arithmoi[17]
+fat = n
+if (fat < p):
+    fat = p
+if (fat < q):
+    fat = q
+if (fat < r):
+    fat = r
+if (fat < s):
+    fat = s
+if (fat < t):
+    fat = t
+
+
+
+
+v = arithmoi[18]
+u = arithmoi[19]
+w = arithmoi[20]
+x = arithmoi[21]
+y = arithmoi[22]
+z = arithmoi[23]
+zed = v
+if (zed < u):
+    zed = u
+if (zed < w):
+    zed = w
+if (zed < x):
+    zed = x
+if (zed < y):
+    zed = y
+if (zed < z):
+    zed = z
+
+
+print('4:00pm', max), ('5:00pm', tra), ('6:00pm', fat), ('7:00pm', zed)
+
+
